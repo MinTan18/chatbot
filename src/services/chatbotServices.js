5ce963341a4b4d9b1938c0184d80ade4cf1e67cf
leminhtan03082000 <74764144+leminhtan03082000@users.noreply.github.com>
2022-04-16 00:19:21 +0700

    update

--- src/services/chatbotServices.js
+++ src/services/chatbotServices.js
@@ -1,10 +1,10 @@
+import { response } from "express";
 import request from "request";
 require("dotenv").config();
 
 const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
 
-
-let callSendAPI = (sender_psid ,response) => {
+let callSendAPI = (sender_psid, response) => {
   // Construct the message body
   let request_body = {
     recipient: {
@@ -30,16 +30,44 @@ let callSendAPI = (sender_psid ,response) => {
     }
   );
 };
+
+let getUserName = async (sender_psid) => {
+    let username = '';
+  // Send the HTTP request to the Messenger Platform
+ await request(
+    {
+      uri: `https://graph.facebook.com/${sender_psid}?fields=first_name,last_name,profile_pic&access_token=${PAGE_ACCESS_TOKEN}`,
+      qs: { access_token: PAGE_ACCESS_TOKEN },
+      method: "GET",
+    },
+    (err, res, body) => {
+      console.log(body);
+      if (!err) {
+        response = JSON.parse(res);
+        username = `${response.first_name} ${response.last_name}`;
+
+        console.log("message sent!");
+      } else {
+        console.error("Unable to send message:" + err);
+      }
+    }
+  );
+
+    return username;
+
+};
+
 let handleGetStarted = (sender_psid) => {
-    return new Promise(async (resolve, reject) => {
-        try {
-            let response = { text: "OK. Welcome to IU Lib System" };
-            await callSendAPI(sender_psid, response);
-            resolve('done');
-        }catch(e) {
-            reject(e);
-        }
-    })
+  return new Promise(async (resolve, reject) => {
+    try {
+        let username = await getUserName(sender_psid)
+      let response = { text: `OK. Welcome ${username} to IU Lib System` };
+      await callSendAPI(sender_psid, response);
+      resolve("done");
+    } catch (e) {
+      reject(e);
+    }
+  });
 };
 
 module.exports = {
