3bb70ab78441cdbde864655798c3e5a37a04ebf8
leminhtan03082000 <74764144+leminhtan03082000@users.noreply.github.com>
2022-04-15 22:43:55 +0700

    update

--- src/controllers/HomeControllers.js
+++ src/controllers/HomeControllers.js
@@ -1,6 +1,8 @@
 require("dotenv").config();
 import request from "request";
 
+const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN
+
 //process.env.NAME.VARIABLES
 let getHomePage = (req, res) => {
   return res.render("homepage.ejs");
@@ -63,64 +65,66 @@ let getWebhook = (req, res) => {
 
 // Handles messages events
 function handleMessage(sender_psid, received_message) {
-    let response;
-  
-    // Checks if the message contains text
-    if (received_message.text) {    
-      // Create the payload for a basic text message, which
-      // will be added to the body of our request to the Send API
-      response = {
-        "text": `You sent the message: "${received_message.text}". Now send me an attachment!`
-      }
-    } else if (received_message.attachments) {
-      // Get the URL of the message attachment
-      let attachment_url = received_message.attachments[0].payload.url;
-      response = {
-        "attachment": {
-          "type": "template",
-          "payload": {
-            "template_type": "generic",
-            "elements": [{
-              "title": "Is this the right picture?",
-              "subtitle": "Tap a button to answer.",
-              "image_url": attachment_url,
-              "buttons": [
+  let response;
+
+  // Checks if the message contains text
+  if (received_message.text) {
+    // Create the payload for a basic text message, which
+    // will be added to the body of our request to the Send API
+    response = {
+      text: `You sent the message: "${received_message.text}". Now send me an attachment!`,
+    };
+  } else if (received_message.attachments) {
+    // Get the URL of the message attachment
+    let attachment_url = received_message.attachments[0].payload.url;
+    response = {
+      attachment: {
+        type: "template",
+        payload: {
+          template_type: "generic",
+          elements: [
+            {
+              title: "Is this the right picture?",
+              subtitle: "Tap a button to answer.",
+              image_url: attachment_url,
+              buttons: [
                 {
-                  "type": "postback",
-                  "title": "Yes!",
-                  "payload": "yes",
+                  type: "postback",
+                  title: "Yes!",
+                  payload: "yes",
                 },
                 {
-                  "type": "postback",
-                  "title": "No!",
-                  "payload": "no",
-                }
+                  type: "postback",
+                  title: "No!",
+                  payload: "no",
+                },
               ],
-            }]
-          }
-        }
-      }
-    } 
-    
-    // Send the response message
-    callSendAPI(sender_psid, response);    
+            },
+          ],
+        },
+      },
+    };
+  }
+
+  // Send the response message
+  callSendAPI(sender_psid, response);
 }
 
 // Handles messaging_postbacks events
 function handlePostback(sender_psid, received_postback) {
-    let response;
-  
-    // Get the payload for the postback
-    let payload = received_postback.payload;
-  
-    // Set the response based on the postback payload
-    if (payload === 'yes') {
-      response = { "text": "Thanks!" }
-    } else if (payload === 'no') {
-      response = { "text": "Oops, try sending another image." }
-    }
-    // Send the message to acknowledge the postback
-    callSendAPI(sender_psid, response);
+  let response;
+
+  // Get the payload for the postback
+  let payload = received_postback.payload;
+
+  // Set the response based on the postback payload
+  if (payload === "yes") {
+    response = { text: "Thanks!" };
+  } else if (payload === "no") {
+    response = { text: "Oops, try sending another image." };
+  }
+  // Send the message to acknowledge the postback
+  callSendAPI(sender_psid, response);
 }
 
 // Sends response messages via the Send API
@@ -151,8 +155,39 @@ function callSendAPI(sender_psid, response) {
   );
 }
 
+let setupProfile = async (req, res) => {
+  // call profile facebook API
+  // Construct the message body
+  let request_body = {
+    "get_started" : {"payload": "GET_STARTED"},
+    "whitelisted_domains" : ["https://chatbot-demo2-prethesis.herokuapp.com/"]
+  };
+
+  // Send the HTTP request to the Messenger Platform
+  await request(
+    {
+      uri: `https://graph.facebook.com/v13.0/me/messenger_profile?access_token=${PAGE_ACCESS_TOKEN}`,
+      qs: { access_token: PAGE_ACCESS_TOKEN },
+      method: "POST",
+      json: request_body,
+    },
+    (err, res, body) => {
+      if (!err) {
+        console.log(body);
+        console.log("Setup user profile succeeds");
+      } else {
+        console.error("Unable to setup:" + err);
+      }
+    }
+  );
+
+    return res.send("Setup succeeds")
+
+};
+
 module.exports = {
   getHomePage: getHomePage,
   getWebhook: getWebhook,
   postWebhook: postWebhook,
+  setupProfile: setupProfile,
 };
