afc3dbc5db51a461d7e9237796e063d0f72462a8
leminhtan03082000 <74764144+leminhtan03082000@users.noreply.github.com>
2022-04-16 09:01:55 +0700

    update

--- src/services/chatbotServices.js
+++ src/services/chatbotServices.js
@@ -35,12 +35,12 @@ let getUserName =  (sender_psid) => {
     return new Promise((resolve, reject) => {
         request(
             {
-              uri: `https://graph.facebook.com/${sender_psid}?fields=first_name,last_name,profile_pic&access_token=${PAGE_ACCESS_TOKEN}`,
-              method: "GET",
+              "uri": `https://graph.facebook.com/${sender_psid}?fields=first_name,last_name,profile_pic&access_token=${PAGE_ACCESS_TOKEN}`,
+              "method": "GET",
             },
             (err, res, body) => {
               if (!err) {
-                response = JSON.parse(body);
+                body = JSON.parse(body);
                 let username = `${body.first_name} ${body.last_name}`;
         
                 resolve(username);
@@ -51,9 +51,6 @@ let getUserName =  (sender_psid) => {
             }
           );
     })
-    
-
-    return username;
 
 };
 
@@ -61,7 +58,7 @@ let handleGetStarted = (sender_psid) => {
   return new Promise(async (resolve, reject) => {
     try {
         let username = await getUserName(sender_psid)
-      let response = { text: `OK. Welcome ${username} to IU Lib System` };
+        let response = { "text": `OK. Welcome ${username} to IU Lib System` };
       await callSendAPI(sender_psid, response);
       resolve("done");
     } catch (e) {
